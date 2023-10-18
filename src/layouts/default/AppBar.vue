d154c1ea4662fbe1da554d4ac6bb1bcfb84fd8e2
Skabeev <vladskab@mail.ru>
2023-10-18 12:00:30 +0300

    init

---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar flat color="primary" density="compact">
+    <v-app-bar-title>
+      Lebed airsoft
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
